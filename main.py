3ad840f0140e3232cf2669e0ce5880a9a71c48e4
c-jijo <cyrilalternative@gmail.com>
2026-07-12 16:35:51 -0500

    Arrange GUI in frames

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfile
 
+customtkinter.set_appearance_mode("dark")
+
 def save_file(textdata): #Writes the textbox data to a text file
         print(textdata)
         file = filedialog.asksaveasfilename(defaultextension=".txt")
@@ -19,14 +21,29 @@ class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.geometry("800x500")
-        self.grid_rowconfigure(0, weight=1) 
-        self.grid_columnconfigure(0, weight=1)
 
-        self.textbox = customtkinter.CTkTextbox(master=self, corner_radius=20, undo=True)
-        self.textbox.grid(padx=30, pady=30, row=0, column=0, sticky="nsew")
-        savebutton = customtkinter.CTkButton(self, text="Save As", command=lambda: save_file(self.textbox.get("0.0", "end"))) #Save As button
+        noteframe = customtkinter.CTkFrame(master=self, border_width=5)
+        tabframe = customtkinter.CTkFrame(master=self, border_width=5)
+        dateframe = customtkinter.CTkFrame(master=self, border_width=5)
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+
+        noteframe.grid(column=1, row=0, sticky="nsew", rowspan=2)
+        noteframe.grid_rowconfigure(0, weight=1)
+        noteframe.grid_columnconfigure(0, weight=1)
+
+        tabframe.grid(column=0, row=0, sticky="nsew")
+        dateframe.grid(column=0, row=1, sticky="nsew" )
+
+        self.textbox = customtkinter.CTkTextbox(master=noteframe, corner_radius=20, undo=True, border_width=5, font=(None, 24))
+        self.textbox.grid(padx=30, pady=30, sticky="nsew")
+        savebutton = customtkinter.CTkButton(noteframe, text="Save As", command=lambda: save_file(self.textbox.get("0.0", "end")), fg_color="grey") #Save As button
         savebutton.grid(padx=0, pady=0)
-        openbutton = customtkinter.CTkButton(self, text="Open", command=lambda:open_file(self.textbox)) #Open button
+        openbutton = customtkinter.CTkButton(noteframe, text="Open", command=lambda:open_file(self.textbox), fg_color="grey") #Open button
         openbutton.grid(padx=0, pady=0)
 
 app = App()
