774f36cc9290c5b37698f560d3f4493197eb3b12
c-jijo <cyrilalternative@gmail.com>
2026-07-12 10:03:34 -0500

    Add basic textbox

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 import customtkinter
+import tkinter
+
+class Textbox(customtkinter.CTkTextbox):
+    def __init__(self, master):
+        super().__init__(master)
+        self.textbox = customtkinter.CTkTextbox(master=self, height=500, width=300, corner_radius=20, undo=True)
+        self.textbox.grid(row=0, column=0, sticky="nsew")
+        
+
+    
+        
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.grid_rowconfigure(0, weight=1) 
+        self.grid_columnconfigure(0, weight=1)
+
+        self.textbox = Textbox(master=self)
+        self.textbox.grid(padx=30, pady=30, sticky="nsew")
+
+
+app = App()
+app.mainloop()
